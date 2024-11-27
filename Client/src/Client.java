3587c00e3f21c831e65f0d6e022c61a96db03792
TeoAnz06 <t3o2066@gmail.com>
2024-11-27 10:12:51 +0100

    Aggiornamento Client, aggiunta Stato

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -35,12 +35,15 @@ public class Client {
             System.exit(0);
 
     //INIZIO DEL MIO TURNO
-        Stato statoPartita = new Stato();
+        Stato statoPartita = null;
         out.writeObject(new Message("STATO", playerId, statoPartita));  //Chiedo al server le carte, che verranno inserite nell'attributo mano
         risposta = (Message) in.readObject();
         
         if(risposta.getStatusCode() != 200)  //Se la risposta è un errore rifaccio la richiesta
             System.exit(0);
+        
+        if(risposta.getOggetto() instanceof Stato)
+            statoPartita = (Stato) risposta.getOggetto();
 
         giocatore = (Giocatore) null;  //Lo ricavo dalòlo stato della partita
         Mano manoDealer = null; //La ricavo poi dallo stato della partita
@@ -48,51 +51,14 @@ public class Client {
         //Stampa dello stato
         System.out.println(statoPartita.toString());
 
-        System.out.println("Tua mano: \n" + giocatore.getMano().toString());  //Stampa mano
         System.out.println("Mano dealer: \n" + manoDealer.toString());
 
     //Scelta mosse
-        int statoRisposta = 0;
-        String mossa = "";
-
-        while(!mossa.equals("STAY")){
+        Giocatore giocatore2 = null;
+        giocata(giocatore, giocatore2, input, out, in);
         
-            System.out.println("Inserisci quale mossa vuoi fare: HIT | STAI | DOUBLE | SPLIT | INSURANCE");  //Chiedo quale azione vuole eseguire e al limite la invio al server
-            mossa = input.readLine();
-            switch(mossa){
-                case "HIT":
-                    out.writeObject(new Message("HIT", playerId, null));
-                    risposta = (Message) in.readObject();
-                    if(risposta.getStatusCode() == 200){  //Se la richiesta è andata a buon fine
-                        if(risposta.getOggetto() instanceof Carta)
-                            giocatore.getMano().add((Carta) risposta.getOggetto());  //Aggiungo alla mia mano la carta che mi ha dato il server
-                    }
-                    break;
-                case "STAY":
-                    out.writeObject(new Message("STAY", playerId, giocatore));
-                    risposta = (Message) in.readObject();
-                    if(risposta.getStatusCode() != 200)
-                        System.exit(0);
-                    break;
-                case "DOUBLE":
-                    out.writeObject(new Message("DOUBLE", playerId, giocatore));
-                    risposta = (Message) in.readObject();
-                    break;
-                case "SPLIT":
-                    out.writeObject(new Message("SPLIT", playerId, giocatore));
-                    risposta = (Message) in.readObject();
-                    break;
-                case "INSURANCE":
-                    out.writeObject(new Message("INSURANCE", playerId, giocatore));
-                    risposta = (Message) in.readObject();
-                    break;
-                default:
-                    statoRisposta = 400;
-            }
-            if(statoRisposta == 300){
-                System.out.println("Azione non consentita\n");
-            }
-        }
+        if(giocatore2 != null)
+            giocata(giocatore2, null, input, out, in);
 
     //Richiesta se ho vinto o no
         out.writeObject(new Message("VITTORIA", playerId, null));
@@ -114,5 +80,81 @@ public class Client {
         out.close();
         in.close();
         input.close();
+    }
+
+    public static void giocata(Giocatore giocatore, Giocatore giocatore2, BufferedReader input, ObjectOutputStream out, ObjectInputStream in) throws Exception{
+        String mossa = "";
+        Message risposta = null;
+        boolean insurance = false;
+
+        while(!mossa.equals("STAY")){
+            
+            System.out.println("Tua mano: \n" + giocatore.getMano().toString());  //Stampa mano
+            System.out.println("Inserisci quale mossa vuoi fare: HIT | STAI | DOUBLE | SPLIT | INSURANCE");  //Chiedo quale azione vuole eseguire e al limite la invio al server
+            mossa = input.readLine();
+            risposta = null;
+
+            switch(mossa){
+                case "HIT":
+                    out.writeObject(new Message("HIT", giocatore.getPlayerId(), null));
+                    risposta = (Message) in.readObject();  //Nel messaggio ricevo la carta che ho chiesto
+                    if(risposta.getStatusCode() == 200){  //Controllo se la richiesta è andata a buon fine
+                        if(risposta.getOggetto() instanceof Carta)
+                            giocatore.getMano().add((Carta) risposta.getOggetto());  //Aggiungo alla mia mano la carta che mi ha dato il server
+                    }
+                    break;
+
+                case "STAY":
+                    out.writeObject(new Message("STAY", giocatore.getPlayerId(), giocatore));
+                    risposta = (Message) in.readObject();
+                    if(risposta.getStatusCode() != 200)
+                        System.exit(0);
+                    break;
+
+                case "DOUBLE":
+                    out.writeObject(new Message("DOUBLE", giocatore.getPlayerId(), null));
+                    risposta = (Message) in.readObject();  //Nel messaggio ricevo la carta aggiuntiva
+                    if(risposta.getStatusCode() == 200){  //Controllo se la richiesta è andata a buon fine
+                        if(risposta.getOggetto() instanceof Carta)
+                            giocatore.getMano().add((Carta) risposta.getOggetto());  //Aggiungo alla mia mano la carta che mi ha dato il server
+                        mossa = "STAY";  //'Sto' in automatico
+                    }
+                    break;
+
+                case "SPLIT":
+                    out.writeObject(new Message("SPLIT", giocatore.getPlayerId(), null));
+                    risposta = (Message) in.readObject();
+                    if(risposta.getStatusCode() == 200){
+                        if(risposta.getOggetto() instanceof Giocatore[]){
+                            Giocatore[] giocatori = (Giocatore[]) risposta.getOggetto();
+                            giocatore = giocatori[0];
+                            giocatore2 = giocatori[1];
+                        }
+                    }
+                    break;
+
+                case "INSURANCE":
+                    if(insurance){
+                        System.out.println("Hai già effettuato un'insurance in precedenza");
+                        break;
+                    }
+
+                    insurance = true;
+                    out.writeObject(new Message("INSURANCE", giocatore.getPlayerId(), null));
+                    risposta = (Message) in.readObject();
+                    if(risposta.getStatusCode() != 200)
+                        System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Metodo inesistente");
+            }
+
+            if(risposta != null){
+                if(risposta.getStatusCode() == 300){
+                    System.out.println("Azione non consentita\n");
+                }
+            }
+        }
     }
 }
--- Client/src/classes/Stato.java
+++ Client/src/classes/Stato.java
@@ -1,5 +1,17 @@
 package classes;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Stato {
     //TODO IMPLEMENTA CLASSE STATO DEL GIOCO
+    private ArrayList<Giocatore> giocatori;
+    private Mano dealerMano; //TODO pensare di nascondere la carta coperta del dealer
+    private HashMap<String,Integer> punteggi;
+
+    public Stato(ArrayList<Giocatore> giocatori,Mano dealerMano,HashMap<String,Integer> punteggi){
+        this.giocatori=giocatori;
+        this.dealerMano=dealerMano;
+        this.punteggi=punteggi;
+    }
 }
