2ee6b64585f7db91439f0866f5582e4bc5b85cd4
TeoAnz06 <109555761+TeoAnz06@users.noreply.github.com>
2024-11-27 08:35:05 +0100

    Aggiornamento client

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -36,68 +36,85 @@ public class Client {
             System.exit(0);   
       
         String playerId = risposta.getPlayerId();
+        Giocatore giocatore = null;
 
-        Giocatore giocatore = new Giocatore();
     //Partita
-        out.writeObject(new Message("FINE", playerId, giocatore));
-        while(((Message) in.readObject()).getStatusCode() == 201){  //Finché non ho finito di giocare  chiedo al server se è il mio turno
-            do{
-                out.writeObject(new Message("TURNO", playerId, giocatore));
-            } while(((Message)in.readObject()).getStatusCode() != 200);  //Finchè il server non invia la conferma del turno
+        do{
+            out.writeObject(new Message("TURNO", playerId, giocatore));
+        } while(((Message) in.readObject()).getStatusCode() != 200);  //Finchè il server non invia la conferma del turno
 
-        //INIZIO DEL MIO TURNO
-            out.writeObject(new Message("MANO", playerId, giocatore));  //Chiedo al server le carte, che verranno inserite nell'attributo mano
-            risposta = (Message) in.readObject();
-            
-            if(risposta.getStatusCode() == 400)  //Se la risposta è un errore rifaccio la richiesta
-                System.exit(0);
+    //INIZIO DEL MIO TURNO
+        Stato statoPartita = new Stato();
+        out.writeObject(new Message("STATO", playerId, statoPartita));  //Chiedo al server le carte, che verranno inserite nell'attributo mano
+        risposta = (Message) in.readObject();
+        
+        if(risposta.getStatusCode() != 200)  //Se la risposta è un errore rifaccio la richiesta
+            System.exit(0);
 
-            giocatore = (Giocatore) risposta.getOggetto();
+        giocatore = (Giocatore) null;  //Lo ricavo dalòlo stato della partita
+        Mano manoDealer = null; //La ricavo poi dallo stato della partita
 
-            System.out.println("Tua mano: \n" + giocatore.getMano().toString());  //Stampa mano
-            Mano manoDealer = new Mano();
-            out.writeObject(new Message("MANO", playerId, manoDealer));
-            System.out.println("Mano dealer: \n" + manoDealer.toString());
+        //Stampa dello stato
+        Sytstem.out.println(statoPartita.toString());
 
-            int statoRisposta = 0;
-            while(statoRisposta != 200){
+        System.out.println("Tua mano: \n" + giocatore.getMano().toString());  //Stampa mano
+        System.out.println("Mano dealer: \n" + manoDealer.toString());
 
-            //Chiedo quale azione vuole eseguire e al limite la invio al server
-                System.out.println("Inserisci quale mossa vuoi fare: CARTA | STAI | RADDOPPIO | SPLIT | ASSICURAZIONE");
-                String mossa = input.readLine();
-                switch(mossa){
-                    case "CARTA":
-                        out.writeObject(new Message("HIT", playerId, giocatore));
-                        statoRisposta = ((Message) in.readObject()).getStatusCode();
-                        break;
-                    case "STAI":
-                        out.writeObject(new Message("STAI", playerId, giocatore));
-                        statoRisposta = ((Message) in.readObject()).getStatusCode();
-                        break;
-                    case "RADDOPPIO":
-                        out.writeObject(new Message("RADDOPPIO", playerId, giocatore));
-                        statoRisposta = ((Message) in.readObject()).getStatusCode();
-                        break;
-                    case "SPLIT":
-                        out.writeObject(new Message("SPLIT", playerId, giocatore));
-                        statoRisposta = ((Message) in.readObject()).getStatusCode();
-                        break;
-                    case "ASSICURAZIONE":
-                        out.writeObject(new Message("ASSICURAZIONE", playerId, giocatore));
-                        statoRisposta = ((Message) in.readObject()).getStatusCode();
-                        break;
-                    default:
-                        statoRisposta = 400;
-                }
+    //Scelta mosse
+        int statoRisposta = 0;
+        String mossa = "";
+
+        while(!mossa.equals("STAY")){
+        
+            System.out.println("Inserisci quale mossa vuoi fare: HIT | STAI | DOUBLE | SPLIT | INSURANCE");  //Chiedo quale azione vuole eseguire e al limite la invio al server
+            mossa = input.readLine();
+            switch(mossa){
+                case "HIT":
+                    out.writeObject(new Message("HIT", playerId, giocatore));
+                    statoRisposta = ((Message) in.readObject()).getStatusCode();
+                    break;
+                case "STAY":
+                    out.writeObject(new Message("STAY", playerId, giocatore));
+                    statoRisposta = ((Message) in.readObject()).getStatusCode();
+                    break;
+                case "DOUBLE":
+                    out.writeObject(new Message("DOUBLE", playerId, giocatore));
+                    statoRisposta = ((Message) in.readObject()).getStatusCode();
+                    break;
+                case "SPLIT":
+                    out.writeObject(new Message("SPLIT", playerId, giocatore));
+                    statoRisposta = ((Message) in.readObject()).getStatusCode();
+                    break;
+                case "INSURANCE":
+                    out.writeObject(new Message("INSURANCE", playerId, giocatore));
+                    statoRisposta = ((Message) in.readObject()).getStatusCode();
+                    break;
+                default:
+                    statoRisposta = 400;
             }
-
-            out.writeObject(new Message("FINE", playerId, giocatore));  //Chiedo al server se non posso più fare mosse
+            if(statoRisposta == 300){
+                System.out.println("Azione non consentita\n");
+            }
         }
 
+    //Richiesta se ho vinto o no
         out.writeObject(new Message("VITTORIA", playerId, giocatore));
-        if(((Message)in.readObject()).getStatusCode() == 210)
+        if(((Message) in.readObject()).getStatusCode() == 210)
             System.out.println("Hai vinto questo round");
         else
             System.out.println("Hai perso questo round");
+
+    //Richiesta dello stato della partita
+        out.writeObject(new Message("STATO", playerId, statoPartita));
+        risposta = (Message) in.readObject();
+        if(risposta.getStatusCode() == 200)
+            System.out.println(((Stato) risposta.getObject()).toString());
+        else   
+            System.out.println("Errore");
+
+        socket.close();
+        out.close();
+        in.close();
+        input.close();
     }
 }
